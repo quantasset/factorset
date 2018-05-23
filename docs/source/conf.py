1530a8f1bd30a0e14cabb294c022a08833e11435
Code37 <495673131@qq.com>
2018-05-23 11:02:07 +0800

    autodoc_mock_imports add tushare.stock

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,7 +52,7 @@ extensions = [
     'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
 ]
-autodoc_mock_imports = ['aiohttp','async_timeout','tushare']
+autodoc_mock_imports = ['aiohttp','async_timeout','tushare', 'tushare.stock']
 
 # -- Docstrings
 extensions += ['sphinx.ext.napoleon']
